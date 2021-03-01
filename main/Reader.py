ee8eb97b3f06c9c1b014a1d672203fcef06ff130
elvirasru <elvira_14.93@hotmail.com>
2021-03-01 20:10:18 +0100

    Add functions to read files

--- main/Reader.py
+++ main/Reader.py
@@ -1,5 +1,29 @@
 import pandas as pd
 
-csv = pd.read_csv("data/vfdb/signal/418.csv", sep='\t', header=None, index_col=0)
 
-print(csv.head())
+def read_signal_annotations(user_id, db):
+    signal = read_signal(user_id, db)
+    annotations = read_annotations(user_id, db)
+
+    new_data = pd.merge(signal, annotations, left_index=True, right_on="signalId", how='left')
+    new_data.reset_index(inplace=True)
+    new_data.drop('index', axis=1, inplace=True)
+    new_data.an5.fillna(method='ffill', inplace=True)
+    return new_data
+
+
+def read_signal(user_id, db):
+    column_names = ['signal']
+    if db == 'vfdb':
+        column_names.append("signal 2")
+    return pd.read_csv("data/" + db + "/signal/" + user_id + ".csv", sep='\t', names=column_names, index_col=0)
+
+
+def read_annotations(user_id, db):
+    df = pd.read_csv("data/" + db + "/annotations/" + user_id + "n.csv", delimiter='\t', names=list('ab'))
+
+    df = df.assign(date=df.a.str.split(expand=True)[0],
+                   signalId=df.a.str.split(expand=True)[1].astype("int64"),
+                   an5=df.b)
+    df.drop(['a', 'b'], axis=1, inplace=True)
+    return df
