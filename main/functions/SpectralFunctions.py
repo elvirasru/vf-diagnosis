0f27cd8cc43032628741d6fe6d79c6ad3a240b4a
danielopez <daniel.lopez.ds@immune.institute>
2021-05-14 12:07:31 -0500

    X1 and bwt

--- main/functions/SpectralFunctions.py
+++ main/functions/SpectralFunctions.py
@@ -18,18 +18,19 @@ def bwt(sub_signal, frequency):
     length = len(sub_signal)
     window_duration = 2
     window_length = 2 * frequency
-    valC = 0.0055
     valPT = 47
 
     x_d = np.ones(1, window_duration * frequency) * sub_signal[:], np.transpose(
-        np.ones(1, window_duration * frequency) * sub_signal[-1])
-    b, a = scipy.signal.butter(5, 2 * [6.5, 30] / frequency, 'bandpass')
+        np.ones(1, window_duration * frequency) * sub_signal[0:length -1])
+    b, a = scipy.signal.butter(5, 2 * [6.5, 30] / frequency, btype="bandpass")
     x_d = scipy.signal.filtfilt(b, a, x_d)
-    x_d = x_d(window_duration * frequency + x_d[0:length - 1] - window_duration * frequency)
-    number_of_windows = round(len(x_dl) / window_length)
+    x_d(window_duration * frequency + x_d[0:] - window_duration * frequency)
+
+    number_of_windows = round(len(x_d) / window_length)
 
     bWT = []
     for i in range(1, number_of_windows):
         window = x_d[i * window_length:(i + 1) * window_length]
         bWT.append((np.percentile(window, 50 + valPT / 2) - np.percentile(window, 50 - valPT / 2)) / max(abs(window)))
+
     return max(bWT)
